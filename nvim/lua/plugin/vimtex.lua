5bc9050c9de8b5cdb92bd5227f8612ece07569ee
river-li <lizic0228@gmail.com>
2024-02-11 10:41:26 -0500

    add config for vimtex

---
+++ nvim/lua/plugin/vimtex.lua
@@ -0,0 +1,6 @@
+vim.cmd("filetype plugin indent on")
+vim.cmd("syntax enable")
+vim.cmd("let g:vimtex_view_method = 'skim'")
+vim.cmd("let g:vimtex_compiler_latexmk_engines = { '_': '-xelatex'}")
+vim.cmd("let g:vimtex_quickfix_open_on_warning = 0")
+vim.cmd("let g:vimtex_view_enabled = 0")
